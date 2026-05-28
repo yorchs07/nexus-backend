c12a2f848324e9e2b283b63470c54b753df5c064
Jorge Espinoza <jorgeanttonio_10@hotmail.com>
2026-05-28 12:50:36 +0200

    Ruta health check añadida

--- src/index.js
+++ src/index.js
@@ -23,6 +23,10 @@ app.use(
   })
 )
 
+app.get('/health', (req, res) => {
+  res.json({ status: 'ok' })
+})
+
 //Parsear JSON + formularios
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
